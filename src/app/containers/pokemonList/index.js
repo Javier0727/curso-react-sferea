1d7b713f50005abc411306761c7623beaf1bcb24
Javier Castillo <y.javier.castillo@gmail.com>
2020-08-22 18:19:40 -0500

    resolver

--- src/app/containers/pokemonList/index.js
+++ src/app/containers/pokemonList/index.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as Actions from '../../store/actions/index'
+import Grid from '@material-ui/core/Grid';
 
 const PokemonList = () => {
   const dispatch = useDispatch()
-  const pokeList = useSelector(({ pokemons }) => pokemons.pokemons)
+  const pokemons = useSelector(({ pokemons }) => pokemons.pokemons)
   const totalPokemons = useSelector(({ pokemons }) => pokemons.totalPokemons)
 
   React.useEffect(() => {
-    dispatch(Actions.getPokeList())
+    
   }, [])
 
   return (
-    // TODO: Lista pokemon donde se va a mapear el arreglo de objetos
-    <div>
+    <div className='px-5 py-4'>
       Hay {totalPokemons}
+      <Grid container spacing={3} className='mt-10'>
+
+        {pokemons.map(pokemon => (
+          <Grid item xs={6}>
+            Nombre
+          </Grid>
+        ))}
+
+      </Grid>
     </div>
   )
 }
